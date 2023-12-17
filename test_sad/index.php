d16199ab475be95b720b948a9749c54fdd3961e3
juliustaha123 <105410360+juliustaha123@users.noreply.github.com>
2023-12-17 22:17:26 +0800

    Update index.php

--- test_sad/index.php
+++ test_sad/index.php
@@ -14,27 +14,43 @@
     <div class="second-top-container">
 
     <form action="process_form.php" method="post">
+       
     <div class="inner-first">
+        
         <label for="firstname">First name </label>
-        <input type="text" name="name" required>
+        <input type="text" name="firstname" required>
         
         <label for="MI">MI </label>
-        <input type="MI" name="MI" required>
+        <input type="text" name="MI" required>
         
         <label for="lastname">Last Name </label>
-        <input type="lastname" name="lastname" required>
+        <input type="text" name="lastname" required>
     </div>
+    <div class="head-container"> 
+        
+        <label for="course">Course </label>
+        <input type="text" name="course" required>
+
+        <label for="yearlevel">Year Level </label>
+        <input type="text" name="yearlevel" required>
+    </div>
+    
 
     <div class="inner-second">
+
         <label for="studentaddress">Student Address </label>
-        <input type="text" name="name" required>
+        <input type="text" name="studentaddress" required>
         
+        <label for="guardianName">Guardian's Name   </label>
+        <input type="tel"  pattern="[0-9]{10}" name="guardianName" required>
+
         <label for="cpNumber">Guardian's Contact No.  </label>
-        <input type="cpNumber" name="cpNumber" required>
+        <input type="tel"  pattern="[0-9]{10}" name="cpNumber" required>
         
         <label for="birthdate">Student's Birth Date</label>
-        <input type="date" id="birthday" name="birthday">
+        <input type="date" id="birthday" name="birthdate">
     </div>
+    
 
     <div class="submit-container">
         <input type="submit" value="Submit">
