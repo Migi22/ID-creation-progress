813d093767d9d078c34faef3e35e905191d5ac0e
Migi22 <108279314+Migi22@users.noreply.github.com>
2023-12-18 15:50:20 +0800

    Add files via upload
    Beta v3.1
    1.) fix the issue of the image is not displaying properly when generating the ID

---
+++ SAD project/SAD project/id_generation_test/id_generation_test/Form1.vb
@@ -0,0 +1,410 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.IO
+
+Public Class Form1
+    Private queueIdToEdit As Integer = -1 ' Variable to store the user ID being edited
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        ' Load all data into DataGridView
+        LoadDataIntoDataGridView()
+    End Sub
+
+    'DATAGRIDVIEW
+
+    Private Sub dgvUsers_SelectionChanged(sender As Object, e As EventArgs) Handles dgvUsers.SelectionChanged
+        ' Handle the selection change in the DataGridView
+        If dgvUsers.SelectedRows.Count > 0 Then
+            ' Get the selected user's ID
+            Dim queue_ID As Integer = Convert.ToInt32(dgvUsers.SelectedRows(0).Cells("queue_ID").Value)
+
+            ' Load user data based on the selected ID
+            LoadUserDataById(queue_ID)
+        End If
+    End Sub
+
+    'Click feature for data grid
+
+    Private Sub dgvUsers_CellMouseClick(sender As Object, e As DataGridViewCellMouseEventArgs) Handles dgvUsers.CellMouseClick
+        Try
+            With dgvUsers
+                txtQueue_ID.Text = .CurrentRow.Cells("queue_ID").Value.ToString
+                txtFname.Text = .CurrentRow.Cells("fname").Value.ToString
+                txtLname.Text = .CurrentRow.Cells("lname").Value.ToString
+                txtMi.Text = .CurrentRow.Cells("m_i").Value.ToString
+                txtCourse.Text = .CurrentRow.Cells("course").Value.ToString
+                txtYearlevel.Text = .CurrentRow.Cells("year_level").Value.ToString
+                txtGuardianName.Text = .CurrentRow.Cells("guardian_name").Value.ToString
+                txtGuardianContactNumber.Text = .CurrentRow.Cells("guardian_contact_number").Value.ToString
+                txtAddress.Text = .CurrentRow.Cells("address").Value.ToString
+                txtBirthday.Text = .CurrentRow.Cells("student_Birthday").Value.ToString
+                txtStudentNumber.Text = .CurrentRow.Cells("student_number").Value.ToString
+                LoadImage()
+            End With
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    'BUTTONS
+
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        ' Load user data based on the queue_ID
+        If Not String.IsNullOrEmpty(txtSelectQueue_ID.Text) Then
+            Dim selectedQueue_ID As Integer
+            If Integer.TryParse(txtSelectQueue_ID.Text, selectedQueue_ID) Then
+                LoadUserDataById(selectedQueue_ID)
+            Else
+                MessageBox.Show("Invalid queue_ID. Please enter a valid numeric ID.")
+            End If
+        End If
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        ' Save changes to the database
+        SaveChangesToDatabase()
+    End Sub
+
+    Private Sub btnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+        'Enable editing
+        EnableEditing()
+    End Sub
+
+    'LOAD DATA INTO DATA GRID VIEW
+
+    Private Sub LoadDataIntoDataGridView()
+        Dim connectionString As String = "server=localhost;user=root;password=;database=id_process;"
+        Dim connection As New MySqlConnection(connectionString)
+
+        Try
+            connection.Open()
+
+            ' Perform database operations
+            Dim query As String = "SELECT * FROM id_queue"
+            Dim adapter As New MySqlDataAdapter(query, connection)
+            Dim dataTable As New DataTable()
+            adapter.Fill(dataTable)
+
+            ' Bind DataTable to DataGridView
+            dgvUsers.DataSource = dataTable
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+    'LOAD USER DATA TO THE TEXT BOX
+
+    Private Sub LoadUserDataById(queueID As Integer)
+        Dim connectionString As String = "server=localhost;user=root;password=;database=id_process;"
+        Dim connection As New MySqlConnection(connectionString)
+
+        'debug
+        MessageBox.Show("Proceed")
+
+        Try
+            connection.Open()
+
+            ' Perform database operations to get user data by ID
+            Dim query As String = "SELECT * FROM id_queue WHERE queue_ID = @queue_ID"
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@queue_ID", queueID)
+
+            Dim reader As MySqlDataReader = command.ExecuteReader()
+
+            ' Enable or disable the "Edit" button based on whether data is found
+            btnEdit.Enabled = reader.HasRows
+            btnSave.Enabled = False ' Disable the "Save" button until editing is initiated
+
+            ' Display the result in TextBox
+            If reader.Read() Then
+                txtQueue_ID.Text = reader("queue_ID").ToString()
+                txtFname.Text = reader("fname").ToString()
+                txtLname.Text = reader("lname").ToString()
+                txtMi.Text = reader("m_i").ToString()
+                txtCourse.Text = reader("course").ToString()
+                txtYearlevel.Text = reader("year_level").ToString()
+                txtGuardianName.Text = reader("guardian_name").ToString()
+                txtGuardianContactNumber.Text = reader("guardian_contact_number").ToString()
+                txtAddress.Text = reader("guardian_address").ToString()
+                txtBirthday.Text = reader("student_Birthday").ToString()
+                txtStudentNumber.Text = reader("student_number").ToString()
+                LoadImage()
+
+            Else
+                txtQueue_ID.Text = String.Empty
+                txtFname.Text = String.Empty
+                txtLname.Text = String.Empty
+                txtMi.Text = String.Empty
+                txtCourse.Text = String.Empty
+                txtYearlevel.Text = String.Empty
+                txtGuardianName.Text = String.Empty
+                txtGuardianContactNumber.Text = String.Empty
+                txtAddress.Text = String.Empty
+                txtBirthday.Text = String.Empty
+                txtStudentNumber.Text = String.Empty
+                LoadImage()
+                MessageBox.Show("No data found for the selected user ID.")
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+
+    'THIS WILL ENABLE THE EDITING
+
+    Private Sub EnableEditing()
+        ' Enable editing in the TextBox controls
+        txtFname.ReadOnly = False
+        txtLname.ReadOnly = False
+        txtMi.ReadOnly = False
+        txtCourse.ReadOnly = False
+        txtYearlevel.ReadOnly = False
+        txtGuardianName.ReadOnly = False
+        txtGuardianContactNumber.ReadOnly = False
+        txtAddress.ReadOnly = False
+        txtBirthday.ReadOnly = False
+        txtStudentNumber.ReadOnly = False
+
+
+        ' Store the user ID being edited from the txtSelectID TextBox
+        If Not String.IsNullOrEmpty(txtSelectQueue_ID.Text) Then
+            queueIdToEdit = Convert.ToInt32(txtSelectQueue_ID.Text)
+        End If
+
+        ' Debugging output
+        MessageBox.Show("EnableEditing clicked. queueIdToEdit: " & queueIdToEdit.ToString())
+
+        btnSave.Enabled = True ' Enable the "Save" button
+    End Sub
+
+    'THIS WILL SAVE THE CHANGES TO THE DATABASE
+
+    Private Sub SaveChangesToDatabase()
+        ' Save changes to the database based on the user ID being edited
+        If queueIdToEdit <> -1 Then
+            Dim connectionString As String = "server=localhost;user=root;password=;database=id_process;"
+            Dim connection As New MySqlConnection(connectionString)
+
+            Try
+                connection.Open()
+
+                ' Perform database operations to update user data
+                Dim query As String = "UPDATE id_queue SET fname = @fname, lname = @lname, m_i = @m_i, course = @course, " & _
+                                   "year_level = @year_level, guardian_name = @guardian_name, " & _
+                                   "guardian_contact_number = @guardian_contact_number, " & _
+                                   "guardian_address = @guardian_address, student_Birthday = @student_Birthday, " & _
+                                   "student_number = @student_number WHERE queue_ID = @queue_ID"
+
+                Dim command As New MySqlCommand(query, connection)
+                command.Parameters.AddWithValue("@fname", txtFname.Text)
+                command.Parameters.AddWithValue("@lname", txtLname.Text)
+                command.Parameters.AddWithValue("@m_i", txtMi.Text)
+                command.Parameters.AddWithValue("@course", txtCourse.Text)
+                command.Parameters.AddWithValue("@year_level", txtYearlevel.Text)
+                command.Parameters.AddWithValue("@guardian_name", txtGuardianName.Text)
+                command.Parameters.AddWithValue("@guardian_contact_number", txtGuardianContactNumber.Text)
+                command.Parameters.AddWithValue("@guardian_address", txtAddress.Text)
+                command.Parameters.AddWithValue("@student_Birthday", txtBirthday.Text)
+                command.Parameters.AddWithValue("@student_number", txtStudentNumber.Text)
+                command.Parameters.AddWithValue("@queue_ID", queueIdToEdit)
+
+                'debug
+                MessageBox.Show(txtStudentNumber.Text)
+
+                command.ExecuteNonQuery()
+
+                ' Disable editing after saving changes
+                txtFname.ReadOnly = True
+                txtLname.ReadOnly = True
+                txtMi.ReadOnly = True
+                txtCourse.ReadOnly = True
+                txtYearlevel.ReadOnly = True
+                txtGuardianName.ReadOnly = True
+                txtGuardianContactNumber.ReadOnly = True
+                txtAddress.ReadOnly = True
+                txtBirthday.ReadOnly = True
+                txtStudentNumber.ReadOnly = True
+                btnSave.Enabled = False
+
+                ' Refresh the DataGridView
+                LoadDataIntoDataGridView()
+
+            Catch ex As Exception
+
+                MessageBox.Show("Error: " & ex.Message)
+            Finally
+                connection.Close()
+            End Try
+        End If
+    End Sub
+
+
+    'BTN browse image
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        Try
+            With OPDPhoto
+                .CheckFileExists = True
+                .CheckPathExists = True
+                .DefaultExt = "jpg"
+                .DereferenceLinks = True
+                .FileName = ""
+                .Filter = "(*.jpg)|*.jpg|(*.png)|*.png|(*.jpg)|*.jpg|All Files|*.*"
+                .Multiselect = False
+                .RestoreDirectory = True
+                .Title = "SELECT FILE TO OPEN"
+                .ValidateNames = True
+
+                If .ShowDialog = Windows.Forms.DialogResult.OK Then
+                    Try
+                        txtPhoto.Text = .FileName
+                        picImportStudent.ImageLocation = .FileName
+                        picImportStudent.SizeMode = PictureBoxSizeMode.StretchImage
+                    Catch fileException As Exception
+                        Throw fileException
+                    End Try
+                End If
+            End With
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    'BTN SAVE IMAGE'
+
+    Private Sub btnSaveImage_Click(sender As Object, e As EventArgs) Handles btnSaveImage.Click
+        ' Enabling the edit
+        EnableEditing()
+
+        Try
+            If String.IsNullOrEmpty(txtPhoto.Text) OrElse String.IsNullOrEmpty(txtFname.Text) Then
+                MessageBox.Show("You need to select an image first", "Required", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            ElseIf Directory.Exists(Application.StartupPath & "\Profiles\" & txtFname.Text & ".png") Then
+                Directory.Delete(txtFname.Text)
+            Else
+                picImportStudent.Image.Save(Application.StartupPath & "\Profiles\" & txtFname.Text & ".png")
+
+                If queueIdToEdit <> -1 Then
+                    Dim connectionString As String = "server=localhost;user=root;password=;database=id_process;"
+                    Dim connection As New MySqlConnection(connectionString)
+
+                    Try
+                        connection.Open()
+
+                        ' Perform database operations to update user data
+                        Dim query As String = "UPDATE id_queue SET image = @image WHERE queue_ID = @queue_ID"
+                        Dim command As New MySqlCommand(query, connection)
+                        command.Parameters.AddWithValue("@image", txtFname.Text & ".png")
+                        command.Parameters.AddWithValue("@queue_ID", queueIdToEdit)
+
+                        command.ExecuteNonQuery()
+
+                    Catch ex As Exception
+                        MessageBox.Show("Error: " & ex.Message)
+                    Finally
+                        connection.Close()
+                    End Try
+                End If
+
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    'Load the image of the Student
+
+    Private Sub LoadImage()
+        'enable editing
+        'EnableEditing()
+
+        Dim connectionString As String = "server=localhost;user=root;password=;database=id_process;"
+        Dim connection As New MySqlConnection(connectionString)
+
+        Try
+            connection.Open()
+
+            ' Perform database operations to update user data
+            Dim query As String = "Select image FROM id_queue WHERE queue_ID = @queue_ID"
+            Dim command As New MySqlCommand(query, connection)
+            command.Parameters.AddWithValue("@queue_ID", txtQueue_ID.Text)
+
+            Dim adapter As New MySqlDataAdapter(command)
+            Dim dataTable As New DataTable()
+            adapter.Fill(dataTable)
+
+            MessageBox.Show(dataTable.Rows.Count)
+            MessageBox.Show(txtFname.Text)
+            If dataTable.Rows.Count > 0 Then
+                ' debug
+                MessageBox.Show("Proceed")
+                If String.IsNullOrEmpty(dataTable.Rows(0).Item("image").ToString) Then
+                    picStudent.ImageLocation = Application.StartupPath & "\Profiles\default.png"
+                Else
+                    picStudent.ImageLocation = Application.StartupPath & "\Profiles\" & dataTable.Rows(0).Item("image").ToString
+                End If
+            End If
+
+
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        Finally
+            connection.Close()
+        End Try
+    End Sub
+
+
+    Private Sub btnGenerateIDCard_Click(sender As Object, e As EventArgs) Handles btnGenerateIDCard.Click
+        'debug
+        MessageBox.Show("debug in btngenerate")
+        MessageBox.Show(txtSelectQueue_ID.Text)
+        MessageBox.Show(dgvUsers.SelectedRows.Count)
+
+        If dgvUsers.SelectedRows.Count > 0 Then
+            Dim studentNumber As String = dgvUsers.SelectedRows(0).Cells("student_number").Value.ToString()
+            Dim fullName As String = dgvUsers.SelectedRows(0).Cells("fname").Value.ToString() & " " &
+                                     dgvUsers.SelectedRows(0).Cells("m_i").Value.ToString() & " " &
+                                     dgvUsers.SelectedRows(0).Cells("lname").Value.ToString()
+            Dim courseYear As String = dgvUsers.SelectedRows(0).Cells("course").Value.ToString() & " " &
+                                       dgvUsers.SelectedRows(0).Cells("year_level").Value.ToString()
+            Dim studentPic As String = dgvUsers.SelectedRows(0).Cells("image").Value.ToString()
+
+
+
+            ' Open Form_id_card and pass the information
+            Dim idCardForm As New Form_id_card(studentNumber, fullName, courseYear, studentPic)
+            idCardForm.Show()
+        Else
+            MessageBox.Show("Please select a user to generate the ID card.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+
+    
+    Private Sub dgvUsers_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvUsers.CellClick
+        Dim i As Integer
+        Dim item As String
+        Try
+            With dgvUsers
+
+                If e.RowIndex >= 0 Then
+                    i = .CurrentRow.Index
+                    txtSearchBox.Text = .Rows(i).Cells("student_num").Value.ToString
+                    LoadImage()
+                    item = .Rows(i).Cells("course").Value.ToString
+                End If
+                
+            End With
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
---
+++ SAD project/SAD project/id_generation_test/id_generation_test/Form_id_card.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form_id_card
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblIDcard = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.picProfile = New System.Windows.Forms.PictureBox()
+        Me.picBarCode = New System.Windows.Forms.PictureBox()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.lblCourse_Year = New System.Windows.Forms.Label()
+        Me.lblStudent_num = New System.Windows.Forms.Label()
+        Me.txtFullName = New System.Windows.Forms.TextBox()
+        Me.txtCourse_Year = New System.Windows.Forms.TextBox()
+        Me.txtStudent_num = New System.Windows.Forms.TextBox()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        CType(Me.picProfile, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBarCode, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblIDcard
+        '
+        Me.lblIDcard.BackColor = System.Drawing.Color.DarkOrchid
+        Me.lblIDcard.Dock = System.Windows.Forms.DockStyle.Top
+        Me.lblIDcard.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.lblIDcard.ForeColor = System.Drawing.Color.White
+        Me.lblIDcard.Location = New System.Drawing.Point(0, 0)
+        Me.lblIDcard.Name = "lblIDcard"
+        Me.lblIDcard.Size = New System.Drawing.Size(949, 36)
+        Me.lblIDcard.TabIndex = 0
+        Me.lblIDcard.Text = "Identification Card"
+        Me.lblIDcard.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.DarkOrchid
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel1.Location = New System.Drawing.Point(0, 546)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(949, 5)
+        Me.Panel1.TabIndex = 1
+        '
+        'picProfile
+        '
+        Me.picProfile.Location = New System.Drawing.Point(47, 75)
+        Me.picProfile.Name = "picProfile"
+        Me.picProfile.Size = New System.Drawing.Size(157, 145)
+        Me.picProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picProfile.TabIndex = 2
+        Me.picProfile.TabStop = False
+        '
+        'picBarCode
+        '
+        Me.picBarCode.Location = New System.Drawing.Point(6, 443)
+        Me.picBarCode.Name = "picBarCode"
+        Me.picBarCode.Size = New System.Drawing.Size(931, 96)
+        Me.picBarCode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.picBarCode.TabIndex = 3
+        Me.picBarCode.TabStop = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(230, 75)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(68, 25)
+        Me.lblName.TabIndex = 4
+        Me.lblName.Text = "Name"
+        '
+        'lblCourse_Year
+        '
+        Me.lblCourse_Year.AutoSize = True
+        Me.lblCourse_Year.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCourse_Year.Location = New System.Drawing.Point(230, 125)
+        Me.lblCourse_Year.Name = "lblCourse_Year"
+        Me.lblCourse_Year.Size = New System.Drawing.Size(233, 25)
+        Me.lblCourse_Year.TabIndex = 4
+        Me.lblCourse_Year.Text = "Course and Year Level"
+        '
+        'lblStudent_num
+        '
+        Me.lblStudent_num.AutoSize = True
+        Me.lblStudent_num.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStudent_num.Location = New System.Drawing.Point(230, 177)
+        Me.lblStudent_num.Name = "lblStudent_num"
+        Me.lblStudent_num.Size = New System.Drawing.Size(168, 25)
+        Me.lblStudent_num.TabIndex = 4
+        Me.lblStudent_num.Text = "Student Number"
+        '
+        'txtFullName
+        '
+        Me.txtFullName.BackColor = System.Drawing.SystemColors.Control
+        Me.txtFullName.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtFullName.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtFullName.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.txtFullName.Location = New System.Drawing.Point(479, 75)
+        Me.txtFullName.Name = "txtFullName"
+        Me.txtFullName.Size = New System.Drawing.Size(289, 23)
+        Me.txtFullName.TabIndex = 5
+        '
+        'txtCourse_Year
+        '
+        Me.txtCourse_Year.BackColor = System.Drawing.SystemColors.Control
+        Me.txtCourse_Year.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtCourse_Year.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtCourse_Year.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.txtCourse_Year.Location = New System.Drawing.Point(479, 125)
+        Me.txtCourse_Year.Name = "txtCourse_Year"
+        Me.txtCourse_Year.Size = New System.Drawing.Size(289, 23)
+        Me.txtCourse_Year.TabIndex = 5
+        '
+        'txtStudent_num
+        '
+        Me.txtStudent_num.BackColor = System.Drawing.SystemColors.Control
+        Me.txtStudent_num.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtStudent_num.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper
+        Me.txtStudent_num.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.txtStudent_num.Location = New System.Drawing.Point(479, 177)
+        Me.txtStudent_num.Name = "txtStudent_num"
+        Me.txtStudent_num.Size = New System.Drawing.Size(289, 23)
+        Me.txtStudent_num.TabIndex = 5
+        '
+        'PrintDocument1
+        '
+        '
+        'Form_id_card
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(949, 551)
+        Me.Controls.Add(Me.txtStudent_num)
+        Me.Controls.Add(Me.txtCourse_Year)
+        Me.Controls.Add(Me.txtFullName)
+        Me.Controls.Add(Me.lblStudent_num)
+        Me.Controls.Add(Me.lblCourse_Year)
+        Me.Controls.Add(Me.lblName)
+        Me.Controls.Add(Me.picBarCode)
+        Me.Controls.Add(Me.picProfile)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblIDcard)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form_id_card"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form_id_card"
+        CType(Me.picProfile, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBarCode, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblIDcard As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents picProfile As System.Windows.Forms.PictureBox
+    Friend WithEvents picBarCode As System.Windows.Forms.PictureBox
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents lblCourse_Year As System.Windows.Forms.Label
+    Friend WithEvents lblStudent_num As System.Windows.Forms.Label
+    Friend WithEvents txtFullName As System.Windows.Forms.TextBox
+    Friend WithEvents txtCourse_Year As System.Windows.Forms.TextBox
+    Friend WithEvents txtStudent_num As System.Windows.Forms.TextBox
+    Friend WithEvents PrintDocument1 As System.Drawing.Printing.PrintDocument
+End Class
