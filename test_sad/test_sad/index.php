1d8d3181c00cf05d7b8c8254a6137abb1f729884
juliustaha123 <105410360+juliustaha123@users.noreply.github.com>
2023-12-18 17:39:53 +0800

    Update index.php

--- test_sad/test_sad/index.php
+++ test_sad/test_sad/index.php
@@ -11,10 +11,11 @@
 
      <div class="first-top-container">
         <h1>JMC Identification Card Request</h1>
+      
     <div class="second-top-container">
-
+        
     <form action="process_form.php" method="post">
-       
+    <div class="jmclogo"><img  src="jmc.png" alt="Italian Trulli"></div>
     <div class="inner-first">
         
         <label for="firstname">First name </label>
@@ -26,14 +27,39 @@
         <label for="lastname">Last Name </label>
         <input type="text" name="lastname" required>
     </div>
-    <div class="head-container"> 
-        
+        <div class="head-container"> 
+        <div class="custome-select">
         <label for="course">Course </label>
-        <input type="text" name="course" required>
-
-        <label for="yearlevel">Year Level </label>
-        <input type="text" name="yearlevel" required>
-    </div>
+        <select name="course" id="course">
+            <option value="BECEd">BECEd</option>
+            <option value="BEEd">BEEd</option>
+            <option value="BSA">BSA</option>
+            <option value="BSMA">BSMA</option>
+            <option value="BSTM">BSTM</option>
+            <option value="BSE-CE">BSE-CE</option>
+            <option value="BSE-SE">BSE-SE</option>
+            <option value="BSCrim">BSCrim</option>
+            <option value="BSIT">BSIT</option>
+            <option value="BSEMC">BSEMC</option>
+            <option value="BSN">BSN</option>
+            <option value="BSP">BSP</option>
+            <option value="BSRT">BSRT</option>
+            <option value="BSAg">BSAg</option>
+            <option value="BSBio">BSBio</option>
+            <option value="BSPsych">BSPsych</option>
+            <option value="BSSW">BSSW</option>
+        </select>
+            </div>
+        <div class="custome-select">
+            <label for="yearlevel">Year Level </label>
+        <select name="yearlevel" id="yearlevel">
+            <option value="1st">1st Year</option>
+            <option value="2nd">2nd Year</option>
+            <option value="3rd">3rd Year</option>
+            <option value="4th">4th Year</option>
+        </select>
+        </div>
+        </div>
     
 
     <div class="inner-second">
@@ -44,8 +70,9 @@
         <label for="guardian_name">Guardian's Name   </label>
         <input type="text" name="guardian_name" required>
 
-        <label for="guardian_contact_number">Guardian's Contact No.  </label>
-        <input type="tel"  pattern="[0-9]{11}" name="guardian_contact_number" required>
+        <label for="guardian_contact_number">Guardian's Contact No.</label>
+        <input pattern="[0-9]{11}" name="guardian_contact_number" type="tel" required>
+
         
         <label for="student_Birthday">Student's Birth Date</label>
         <input type="date" id="birthday" name="student_Birthday">
